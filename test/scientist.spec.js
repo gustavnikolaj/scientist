6b23617700bd4daf36339c6df30dab7ff6281a00
Gustav Nikolaj Olsen <gno@one.com>
2019-10-31 18:33:21 +0100

    Initial commit

---
+++ test/scientist.spec.js
@@ -0,0 +1,8 @@
+const expect = require("unexpected");
+const scientist = require("../lib/scientist");
+
+describe("scientist", () => {
+  it("should be a function", () => {
+    expect(scientist, "to be a function");
+  });
+});
